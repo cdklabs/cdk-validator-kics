ac2ec72fc48604b7b866f5e91953e901f66ce83f
corymhall <43035978+corymhall@users.noreply.github.com>
2023-03-14 16:26:32 +0000

    feat: create kics plugin
    TODO: fill in

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,37 @@
 import { CdklabsJsiiProject } from 'cdklabs-projen-project-types';
+import { BundleKics } from './projenrc';
 const project = new CdklabsJsiiProject({
   author: 'AWS',
   authorAddress: 'aws-cdk-dev@amazon.com',
   defaultReleaseBranch: 'main',
-  devDeps: ['cdklabs-projen-project-types'],
+  devDeps: [
+    'cdklabs-projen-project-types',
+    '@octokit/types',
+    '@octokit/rest',
+    'mock-fs',
+    '@types/mock-fs',
+    'fs-extra',
+    '@types/fs-extra',
+  ],
   name: 'cdk-validator-kics',
   projenrcTs: true,
   release: false,
   repositoryUrl: 'https://github.com/cdklabs/cdk-validator-kics.git',
+  deps: [
+    'aws-cdk-lib',
+  ],
+  peerDeps: [
+    'aws-cdk-lib',
+  ],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.tsconfig?.addInclude('projenrc/**/*.ts');
+project.gitignore.exclude('bin');
+project.gitignore.exclude('assets');
+
+new BundleKics(project);
 project.synth();
